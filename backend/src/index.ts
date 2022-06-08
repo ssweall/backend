33a054a7e55b2c5d6b953714e8e371595a029bd8
ssweall <44494824+ssweall@users.noreply.github.com>
2022-06-08 15:43:46 +0200

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,4 +4,4 @@ const port = 3000;
 app.get('/', (_, res) => {
   res.status(200).send('OK');
 });
-app.listen(port, () => console.log(`Running on port ${port}`));
+app.listen(port, () => console.log(`Running port ${port}`));
